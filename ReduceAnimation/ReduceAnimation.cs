acb53d7d31f0414b615a8809a5abadf10607286f
ftfft <17285570+EIA485@users.noreply.github.com>
2025-09-05 18:44:11 -0700

    Port to bepis

--- ReduceAnimation/ReduceAnimation.cs
+++ ReduceAnimation/ReduceAnimation.cs
@@ -1,104 +1,58 @@
-﻿using NeosModLoader;
-using HarmonyLib;
+﻿using BepInEx;
+using BepInEx.NET.Common;
+using BepInExResoniteShim;
 using FrooxEngine;
 using FrooxEngine.UIX;
-using System.Collections.Generic;
-using System.Reflection.Emit;
+using HarmonyLib;
 using System.Reflection;
 
 namespace ReduceAnimation
 {
-    public class ReduceAnimation : NeosMod
+    [ResonitePlugin(PluginMetadata.GUID, PluginMetadata.NAME, PluginMetadata.VERSION, PluginMetadata.AUTHORS, PluginMetadata.REPOSITORY_URL)]
+    [BepInDependency(BepInExResoniteShim.PluginMetadata.GUID)]
+    public class ReduceAnimation : BasePlugin
     {
-        public override string Name => "ReduceAnimation";
-        public override string Author => "eia485";
-        public override string Version => "1.3.0";
-        public override string Link => "https://github.com/EIA485/NeosReduceAnimation";
-        public override void OnEngineInit()
-        {
-            Harmony harmony = new Harmony("net.eia485.ReduceAnimation");
-            harmony.PatchAll();
-
-            MethodInfo OpenCloseSpeedOnAttach = null;
-
-            var ins = PatchProcessor.GetOriginalInstructions(AccessTools.Method(typeof(SettingsDialog), "OnAttach"));
-            for (int i = 0; i < ins.Count; i++)
-            {
-                if (ins[i].opcode == OpCodes.Ldstr && ins[i].operand == "Settings.Dash.OpenCloseSpeed")
-                {
-                    for (int si = i + 1; si < ins.Count; si++)
-                    {
-                        if (ins[si].opcode == OpCodes.Ldftn)
-                        {
-                            OpenCloseSpeedOnAttach = (MethodInfo)ins[si].operand;
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
-            if (OpenCloseSpeedOnAttach != null)
-            {
-                harmony.Patch(OpenCloseSpeedOnAttach, transpiler: new HarmonyMethod(AccessTools.Method(typeof(ReduceAnimation), nameof(DashTranspiler))));
-            }
-        }
-
-        static IEnumerable<CodeInstruction> DashTranspiler(IEnumerable<CodeInstruction> codes)
-        {
-            foreach (var code in codes)
-            {
-                if (code.opcode == OpCodes.Ldc_R4 && (float)code.operand == 10f) code.operand = float.PositiveInfinity;
-                yield return code;
-            }
-        }
-
+        static MethodInfo PositionScreen = AccessTools.Method(typeof(RadiantDash), "PositionScreen");
+        public override void Load() => HarmonyInstance.PatchAll();
 
         [HarmonyPatch]
         class ReduceAnimationPatch
         {
-            [HarmonyPrefix]
-            [HarmonyPatch(typeof(SlideSwapRegion), "Swap")]
-            static void SlideSwapRegionSwapPrefix(ref float duration)
-            {
-                duration = 0;
-            }
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(RadiantDash), "OnDashSettingsUpdated")]
+            static void RadiantDashOnDashSettingsUpdatedPostfix(RadiantDash __instance) => __instance.RunSynchronously(()=>__instance.AnimationSpeed.Value = float.MaxValue);
 
             [HarmonyPrefix]
-            [HarmonyPatch(typeof(NeosSwapCanvasPanel), "SwapPanel")]
-            static void NeosSwapCanvasPanelSwapPanelPrefix(ref float duration)
-            {
-                duration = 0;
-            }
+            [HarmonyPatch(typeof(SlideSwapRegion), nameof(SlideSwapRegion.Swap))]
+            static void SlideSwapRegionSwapPrefix(ref float duration) => duration = 0;
 
-            [HarmonyTranspiler]
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(LegacySwapCanvasPanel), nameof(LegacySwapCanvasPanel.SwapPanel))]
+            static void NeosSwapCanvasPanelSwapPanelPrefix(ref float duration) => duration = 0;
+
+            [HarmonyPostfix]
             [HarmonyPatch(typeof(RadiantDash), "OnCommonUpdate")]
-            static IEnumerable<CodeInstruction> RadiantDash_OnCommonUpdateTranspiler(IEnumerable<CodeInstruction> instructions)
+            static void ModalOverlayOnAwakePostfix(RadiantDash __instance, ref float ____screenLerp, ref RadiantDashScreen ____previousScreen, RadiantDashScreen ____currentScreen)
             {
-                var codes = new List<CodeInstruction>(instructions);
-                for (int i = 0; i < codes.Count - 4; i++)
+                if (____previousScreen != null)
                 {
-                    if (codes[i].opcode == OpCodes.Ldarg_0 & codes[i + 1].opcode == OpCodes.Ldc_R4 & codes[i + 2].opcode == OpCodes.Stfld & codes[i + 3].opcode == OpCodes.Br_S)
-                    {
-                        if ((float)codes[i + 1].operand == 1f)
-                        {
-                            codes[i + 1].operand = 0f;
-                            break;
-                        }
-                    }
+                    ____screenLerp = 0f;
+                    ____previousScreen.Hide();
+                    ____previousScreen = null;
                 }
-                return codes;
+                if (____currentScreen != null)
+                {   //saves 1 frame of time
+                    PositionScreen.Invoke(__instance, new object[] { ____currentScreen, 0f, 0f });
+                }
             }
 
             [HarmonyPostfix]
             [HarmonyPatch(typeof(ModalOverlay), "OnAwake")]
-            static void ModalOverlayOnAwakePostfix(ModalOverlay __instance)
-            {
-                __instance.AnimationTime.Value = 0f;
-            }
+            static void ModalOverlayOnAwakePostfix(ModalOverlay __instance) => __instance.AnimationTime.Value = 0f;
 
             [HarmonyPostfix]
-            [HarmonyPatch(typeof(WorldDetail), "OnChanges")]
-            static void WorldDetailOnChangesPostfix(WorldDetail __instance, ref float ____expandLerp, ref float ____compactDetailLerp)
+            [HarmonyPatch(typeof(LegacyWorldDetail), "OnChanges")]
+            static void WorldDetailOnChangesPostfix(LegacyWorldDetail __instance, ref float ____expandLerp, ref float ____compactDetailLerp)
             {
                 ____expandLerp = (__instance.Expanded.Value ? 1f : 0f);
                 ____compactDetailLerp = (__instance.CompactDetailExpanded.Value ? 1f : 0f);
